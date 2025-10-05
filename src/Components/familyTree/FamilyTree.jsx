ba6a29c352e7db1229e7577968eac83eed3c21cb
AbdurRob <abdurrobr34l@gmail.com>
2025-10-05 20:42:53 +0600

    Practiced react Context

--- src/Components/familyTree/FamilyTree.jsx
+++ src/Components/familyTree/FamilyTree.jsx
@@ -1,20 +1,28 @@
-import React from 'react';
-import GrandFather from './familyMembers/GrandFather';
-import Dad from './familyMembers/Dad';
-import Uncle from './familyMembers/Uncle';
-import Aunt from './familyMembers/Aunt';
+import React, { createContext, useState } from "react";
+import GrandFather from "./familyMembers/GrandFather";
+import Dad from "./familyMembers/Dad";
+import Uncle from "./familyMembers/Uncle";
+import Aunt from "./familyMembers/Aunt";
+
+export const MoneyContext = createContext(0);
 
 const FamilyTree = () => {
+
+  const [money, setMoney] = useState(0)
+
   return (
     <div>
       <h2 className="title">Family Tree</h2>
-      
-      <GrandFather></GrandFather>
-      <section className='childrens'>
-        <Dad></Dad>
-        <Uncle></Uncle>
-        <Aunt></Aunt>
-      </section>
+
+<h5>Total family money: {money}</h5>
+      <MoneyContext value={[money, setMoney]}>
+        <GrandFather></GrandFather>
+        <section className="childrens">
+          <Dad></Dad>
+          <Uncle></Uncle>
+          <Aunt></Aunt>
+        </section>
+      </MoneyContext>
     </div>
   );
 };
--- src/Components/familyTree/familyMembers/Uncle.jsx
+++ src/Components/familyTree/familyMembers/Uncle.jsx
@@ -1,9 +1,14 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { MoneyContext } from '../FamilyTree';
 
 const Uncle = () => {
+
+  const [money, setMoney] = useContext(MoneyContext);
+
   return (
     <div>
       <h2 className="title children">UNCLE</h2>
+      <button onClick={() => setMoney(money + 1000)}>Add 1000tk</button>
     </div>
   );
 };
